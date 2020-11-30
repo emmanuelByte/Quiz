934aba5f064536fb5ddcccce97ea84c2695271fc
EmanuelOne <emanuel4mechie@gmail.com>
2020-11-30 07:11:34 +0100

    Updates

---
+++ precache-manifest.cabdda4980e3a50242f46234767bb4e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bac4a4dc685608364bc369b2a9e292f9",
+    "url": "/Quiz/index.html"
+  },
+  {
+    "revision": "1975b7a490f916fb9ca7",
+    "url": "/Quiz/static/css/main.9f1b6af7.chunk.css"
+  },
+  {
+    "revision": "e19aee5ac5327aa2d561",
+    "url": "/Quiz/static/js/2.219c59ec.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/Quiz/static/js/2.219c59ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1975b7a490f916fb9ca7",
+    "url": "/Quiz/static/js/main.64f26d06.chunk.js"
+  },
+  {
+    "revision": "de149c0bed6658bde3c9",
+    "url": "/Quiz/static/js/runtime-main.00013ed4.js"
+  }
+]);
